b71497dcf0b52d8d8d3dede1d2de7d48c7f83925
Sebastian Chalarca <chalarcasebastian@gmail.com>
2016-11-30 20:58:22 -0500

    bot

--- serve.js
+++ serve.js
@@ -20,21 +20,13 @@ app.get('/', function(req, res) {
 
 // for Facebook verification
 app.get('/webhook/', function(req, res) {
-    messaging_events = req.body.entry[0].messaging;
-    for (i = 0; i < messaging_events.length; i++) {
-        event = req.body.entry[0].messaging[i];
-        sender = event.sender.id;
-        if (event.message && event.message.text) {
-            text = event.message.text;
-            console.log(text);
-        }
+    if (req.query['hub.verify_token'] === 'my_voice_is_my_password_verify_me') {
+        res.send(req.query['hub.challenge'])
     }
-    res.status(200).send(req.query['hub.challenge']);
+    res.send('Error, wrong token')
 })
 
 // Spin up the server
 app.listen(app.get('port'), function() {
-console.log('running on port', app.get('port'))
-})
-
+    console.log('running on port', app.get('port'))
 })
